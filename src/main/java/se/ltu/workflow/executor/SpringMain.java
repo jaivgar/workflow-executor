16660bd5c11203c1bad6eec8caf4616d28cc44be
jaivgar <jaime.garcia@ltu.se>
2020-08-27 16:56:01 +0200

    Fix log4j2 xml file used as configuration and format
    The log4j2 is using to configure the logs the "log4j2.xml" present in
    target directory, not inside jar. In this way the logging can change
    without the need to recompile project.
    
    Also change the pattern format for the log files created. Its pattern
    is esentially the same, but without the color options, as those are for
    displaying nicely in console only.

--- src/main/java/se/ltu/workflow/executor/SpringMain.java
+++ src/main/java/se/ltu/workflow/executor/SpringMain.java
@@ -7,10 +7,13 @@ import org.springframework.context.annotation.ComponentScan;
 import eu.arrowhead.common.CommonConstants;
 
 @SpringBootApplication
-// Not sure yet if I would need the beans from eu.arrowhead
+// Beans from eu.arrowhead used in ApplicationListener
 @ComponentScan(basePackages = {CommonConstants.BASE_PACKAGE, WExecutorConstants.BASE_PACKAGE})
 public class SpringMain {
 
+    static {
+        System.setProperty("log4j.configurationFile", "log4j2.xml");
+    }
     public static void main(final String[] args) {
         /*
          * The code that runs at start up of the system and at shutdown, is located in
